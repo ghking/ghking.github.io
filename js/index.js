b6ecd726dfaf35135bac6eb8d2e56aef080e8e04
Gavin King <gavin@vimeo.com>
2016-12-12 22:04:37 -0500

    copy and asset organization

--- js/index.js
+++ js/index.js
@@ -1,28 +1,46 @@
 $(document).ready(function()
 {
-	$("#work-button").click(function()
-	{
-		didClickNavigationButton($(this), "html/work.html")
-	});
+    setupListeners();
+    setupEmailAddress();
 
-	$("#personal-projects-button").click(function()
-	{
-		didClickNavigationButton($(this), "html/personal_projects.html")
-	});
-
-	$("#education-button").click(function()
-	{
-		didClickNavigationButton($(this), "html/education.html")
-	});
-
-	$("#personal-projects-button").click()
+    $("#personal-projects-button").click();
 });
+
+// Setup
+
+function setupEmailAddress()
+{
+    var name = "hello";
+    var domain = "ghking.co";
+
+    $("#email").attr("href", "mail" + "to" + ":" + name + "@" + domain);
+}
+
+function setupListeners()
+{
+    $("#work-button").click(function()
+    {
+        didClickNavigationButton($(this), "html/work.html");
+    });
+
+    $("#personal-projects-button").click(function()
+    {
+        didClickNavigationButton($(this), "html/personal_projects.html");
+    });
+
+    $("#education-button").click(function()
+    {
+        didClickNavigationButton($(this), "html/education.html");
+    });
+}
+
+// Helpers
 
 function didClickNavigationButton(navigationButton, contentURL)
 {
-	$("nav > .selected").removeClass("selected")
+	$("nav > .selected").removeClass("selected");
 
-	navigationButton.addClass("selected")
+	navigationButton.addClass("selected");
 
 	$("#content").load(contentURL);
 }
