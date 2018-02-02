4d4dc54c547d07e40ef649190d0fe4f9f7d1f06e
Gavin King <ghking13@gmail.com>
2018-02-02 17:40:25 -0500

    what

--- js/index.js
+++ js/index.js
@@ -1,12 +1,7 @@
 $(document).ready(function()
 {
-    setupListeners();
     setupEmailAddress();
-
-    $("#work-button").click();
 });
-
-// Setup
 
 function setupEmailAddress()
 {
@@ -14,33 +9,4 @@ function setupEmailAddress()
     var domain = "ghking.co";
 
     $("#email").attr("href", "mail" + "to" + ":" + name + "@" + domain);
-}
-
-function setupListeners()
-{
-    $("#work-button").click(function()
-    {
-        didClickNavigationButton($(this), "html/work.html");
-    });
-
-    $("#personal-projects-button").click(function()
-    {
-        didClickNavigationButton($(this), "html/personal_projects.html");
-    });
-
-    $("#education-button").click(function()
-    {
-        didClickNavigationButton($(this), "html/education.html");
-    });
-}
-
-// Helpers
-
-function didClickNavigationButton(navigationButton, contentURL)
-{
-	$("nav > .selected").removeClass("selected");
-
-	navigationButton.addClass("selected");
-
-	$("#content").load(contentURL);
 }
